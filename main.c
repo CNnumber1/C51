66573ced8dd2288afde0d8ffd4892b28a856f6ea
1 <115859190+CNnumber1@users.noreply.github.com>
2023-11-23 21:58:12 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,42 +1,42 @@
 #include <REGX52.H>
-#include <INTRINS.H>
 
-void Delay500ms()		//@12.000MHz
+void Delay10ms(unsigned int xms)		//@12.000MHz
 {
-	unsigned char i, j, k;
+	while(xms)
+	{
+			unsigned char i, j;
 
-	_nop_();
-	i = 4;
-	j = 205;
-	k = 187;
+	i = 20;
+	j = 113;
 	do
 	{
-		do
-		{
-			while (--k);
-		} while (--j);
+		while (--j);
 	} while (--i);
+	xms=xms-1;
+}
+
 }
 
 void main()
 {
 	while(1)
 	{
-		P2=0XFE;//1111 1110
-		Delay500ms();
+	    P2=0XFE;//1111 1110
+		Delay10ms(100);
 		P2=0XFD;//1111 1101
-		Delay500ms();  
+		Delay10ms(100);  
 		P2=0XFB;//1111 1011
-		Delay500ms();
+		Delay10ms(100);
 		P2=0XF7;//1111 0111
-		Delay500ms();
+		Delay10ms(100);
 		P2=0XEF;//1110 1111
-		Delay500ms();
+		Delay10ms(100);
 		P2=0XDF;//1101 1111
-		Delay500ms();
+		Delay10ms(100);
 		P2=0XBF;//1011 1111
-		Delay500ms();
+		Delay10ms(100);
 		P2=0X7F;//0111 1111
-		Delay500ms();
+		Delay10ms(100);
 	}
+
 }
