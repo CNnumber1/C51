8f1f5337d8b0620de7166a3da14c4fe3875ad08c
1 <115859190+CNnumber1@users.noreply.github.com>
2023-11-30 23:28:51 +0800

    3.2

--- main.c
+++ main.c
@@ -1,16 +1,33 @@
 #include <REGX52.H>
 
+void Delay(unsigned int xms)	//@12.000MHz
+{
+	unsigned char data i, j;
+	while(xms)
+	{
+		i = 2;
+		j = 239;
+		do
+		{
+			while (--j);
+		} while (--i);
+		xms--;
+	}
+	
+}
+
+
 void main()
 {
-//	P2=0XFE;
-	
-
-
 	while(1)
 	{
 		if(P3_1==0)
-			P2_0=0;
-		else if(P3_1==1)
-			P2_0=1;
+		{
+			Delay(20);
+			while(P3_1==0);
+			Delay(20);
+			
+			P2_0 =~ P2_0;
+		}
 	}
 }
